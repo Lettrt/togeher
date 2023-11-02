26e05e2dd15768dbec132351f2193ee4d6d8d772
anastasniu <nigit uanastasiia@gmail.com>
2023-11-02 19:41:19 +0600

    created login, signup

--- auth_niu/urls.py
+++ auth_niu/urls.py
@@ -3,8 +3,11 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from . import views
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('signup/', views.signup),
+    path('login/', views.login),
 ]
