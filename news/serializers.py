9e096733f84c2650027f2462e71e7a1bcdc6ca5c
Aibek-Kozonberdiev <ratroniii@gmail.com>
2023-11-02 19:28:11 +0600

    update serializer

--- news/serializers.py
+++ news/serializers.py
@@ -1,9 +1,11 @@
+from datetime import datetime
 from rest_framework import serializers
 
 from .models import News
 
 
 class NewsModelSerializer(serializers.ModelSerializer):
+    update_at = serializers.DateTimeField(default=datetime.now())
 
     class Meta:
         model = News
