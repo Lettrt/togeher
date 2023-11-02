87250122b785d90c1ae65b1d7747344fbf1c2fba
EEkaterina19 <bekaterina1996@gmail.com>
2023-11-02 21:18:25 +0600

    fix

--- config/urls.py
+++ config/urls.py
@@ -38,3 +38,21 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    # Admin
+    path('admin/', admin.site.urls),
+
+    # Apps
+    path('api/v1/', include('news.urls')),
+
+    # Auth
+    path('auth/', include('rest_framework.urls')),
+
+    # Token
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
