8606bfb20e5f8ef2f49bbd4a3ca1035a7de3220a
Aibek-Kozonberdiev <ratroniii@gmail.com>
2023-11-02 19:31:34 +0600

    fixed NewsModelSerializer

--- news/serializers.py
+++ news/serializers.py
@@ -1,11 +1,11 @@
-from datetime import datetime
+from django.utils import timezone
 from rest_framework import serializers
 
 from .models import News
 
 
 class NewsModelSerializer(serializers.ModelSerializer):
-    update_at = serializers.DateTimeField(default=datetime.now())
+    update_at = serializers.DateTimeField(default=timezone.now)
 
     class Meta:
         model = News
