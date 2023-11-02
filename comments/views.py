daf3d505b6ddf2931b97804fcc39da7ec960a36b
EEkaterina19 <bekaterina1996@gmail.com>
2023-11-02 19:23:36 +0600

    comment3

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from .models import Comment
+from rest_framework import generics
+from .serializers import CommentSerializers
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated 
+from .permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+
+class CommentAPIView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializers
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+
+
+class CommentAPIUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializers
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly, )
