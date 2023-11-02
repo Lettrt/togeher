29077c29e5ee6295e48cd01a6399f3367d94e0bf
anastasniu <nigit uanastasiia@gmail.com>
2023-11-02 19:12:41 +0600

    installed simplejwt, added urls.py

--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     'djoser',
     'drf_yasg',
     'auth_niu'
+
+
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
